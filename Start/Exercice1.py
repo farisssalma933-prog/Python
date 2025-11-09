5bdfc0204d059139db7ac950d22d412d7bffdcf1
farisssalma933-prog <farisssalma933@gmail.com>
2025-11-09 15:38:26 +0100

    add

---
+++ Start/Exercice1.py
@@ -0,0 +1,15 @@
+
+a = 1
+b = 2
+x = 3
+y = 4
+chaine1 = "HI"
+chaine2 = "WORLD"
+resultat1 = 5 * x + a + b / 4
+print(resultat1, type(resultat1))
+resultat2 = 3 - x % y
+print(resultat2, type(resultat2))
+resultat3 = chaine1 + chaine2
+print(resultat3, type(resultat3))
+resultat4 = len("Salut" + chaine1)
+print(resultat4, type(resultat4))
