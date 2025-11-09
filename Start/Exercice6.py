995575f06851325a54b53102c0927d864a5183b8
farisssalma933-prog <farisssalma933@gmail.com>
2025-11-09 17:07:00 +0100

    Ajout des exercices 6, 7 et 8

---
+++ Start/Exercice6.py
@@ -0,0 +1,11 @@
+A = 1
+B = 2
+C = 3
+D = A
+A = C
+C = B
+B = D
+
+print("A =", A)
+print("B =", B)
+print("C =", C)
---
+++ Start/Exercice8.py
@@ -0,0 +1,10 @@
+A = 30
+B = A * 2
+print("B =", B)
+
+C = (B + A) / 4
+B = C / 5
+D = "Amine"
+E = (A > 40) or (C < B)
+
+print("Les valeurs obtenues sont : A =", A, "B =", B, "C =", C, "D =", D, "E =", E)
